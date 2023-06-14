420bcb2f8e276072491bb1f21f615257c5e1b636
Sean <soxfansean@gmail.com>
2023-06-14 17:49:29 -0400

    Add functions to get random computer choice & displayer winner

---
+++ game.js
@@ -0,0 +1,42 @@
+function getComputerChoice(){
+    const choice = parseInt(Math.random() * 3);
+    let result;
+    if(choice === 0){
+        result = "Rock";
+    }else if(choice === 1){
+        result = "Paper";
+    }else if(choice === 2){
+        result = "Scissors";
+    }
+    return result;
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    let resultMessage;
+    if(playerSelection === computerSelection){
+        resultMessage = "It's a tie!";
+    }else if(playerSelection === "rock"){
+        if(computerSelection === "paper"){
+            resultMessage = "You lose! Paper beats rock!";
+        }else{
+            resultMessage = "You win! Rock beats scissors!";
+        }
+    }else if(playerSelection === "paper"){
+        if(computerSelection === "rock"){
+            resultMessage = "You win! Paper beats rock!";
+        }else{
+            resultMessage = "You lose! Scissors beats paper!";
+        }
+    }else if(playerSelection === "scissors"){
+        if(computerSelection === "rock"){
+            resultMessage = "You lose! Rock beats scissors!";
+        }else{
+            resultMessage = "You win! Scissors beats paper!";
+        }
+    }
+
+    return resultMessage;
+}
